cc25199300d8aad4e28e0e791274cbc3936fe40c
David Zeng <dpz26@drexel.edu>
2020-12-31 11:35:45 -0500

    hw1 upload

---
+++ hw/hw1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#Your task here is to complete all the operations for the two variables below like in the menu
+
+mathVar1 = 3
+mathVar2 = 4
+loopVar = True
+menu = "1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n5. Modulus\n6. Floor\nEnter a number\n"
+
+
+
+while(loopVar):
+    x = input(menu)
+    
