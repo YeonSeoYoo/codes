1c20c91d4f12bacb1466637d975a745c736407ac
David Zeng <dpz26@drexel.edu>
2020-12-31 12:22:42 -0500

    hw2

---
+++ hw/hw2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+#Owen is 7 years old, Andy is 24 years old, Rachel is 20 years old, and Victor is 16 years old
+#Your task is to use only the variables below to print out each person's age when a number is entered
+
+stringVar1 = "Owen is"
+stringVar2 = "Andy is"
+stringVar3 = "Rachel is"
+stringVar4 = "Victor is"
+
+mathVar1 = 10
+mathVar2 = 14
+mathVar3 = 2
+
+space = " "
+
+loopVar = True
+
+menu = "1. Owen's Age\n2. Andy's Age\n3. Rachel's Age\n4. Victor's Age\nEnter a number to find out a person's age\n"
+
+while(loopVar):
+    x = input(menu)
